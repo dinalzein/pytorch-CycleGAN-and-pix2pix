3a9260b4084906455c4f376dd3259d8760b7d76d
dinalzein <dha21@mail.aub.edu>
2022-11-21 11:29:24 +0100

    add aligned loss and masking

--- models/cycle_gan_model.py
+++ models/cycle_gan_model.py
@@ -3,7 +3,11 @@ import itertools
 from util.image_pool import ImagePool
 from .base_model import BaseModel
 from . import networks
-
+from skimage.draw import polygon
+import numpy as np
+from shapely.geometry import Polygon
+import sys
+import random
 
 class CycleGANModel(BaseModel):
     """
@@ -40,7 +44,7 @@ class CycleGANModel(BaseModel):
         if is_train:
             parser.add_argument('--lambda_A', type=float, default=10.0, help='weight for cycle loss (A -> B -> A)')
             parser.add_argument('--lambda_B', type=float, default=10.0, help='weight for cycle loss (B -> A -> B)')
-            parser.add_argument('--lambda_identity', type=float, default=0.5, help='use identity mapping. Setting lambda_identity other than 0 has an effect of scaling the weight of the identity mapping loss. For example, if the weight of the identity loss should be 10 times smaller than the weight of the reconstruction loss, please set lambda_identity = 0.1')
+            parser.add_argument('--lambda_identity', type=float, default=0, help='use identity mapping. Setting lambda_identity other than 0 has an effect of scaling the weight of the identity mapping loss. For example, if the weight of the identity loss should be 10 times smaller than the weight of the reconstruction loss, please set lambda_identity = 0.1')
 
         return parser
 
@@ -56,9 +60,16 @@ class CycleGANModel(BaseModel):
         # specify the images you want to save/display. The training/test scripts will call <BaseModel.get_current_visuals>
         visual_names_A = ['real_A', 'fake_B', 'rec_A']
         visual_names_B = ['real_B', 'fake_A', 'rec_B']
+        if self.isTrain:
+            visual_names_A.append('fake_BM')
+            visual_names_B.append('real_BM')
+            visual_names_B.append('rec_BM')
+            visual_names_B.append('M')
+
         if self.isTrain and self.opt.lambda_identity > 0.0:  # if identity loss is used, we also visualize idt_B=G_A(B) ad idt_A=G_A(B)
             visual_names_A.append('idt_B')
             visual_names_B.append('idt_A')
+            visual_names_B.append('idt_AM')
 
         self.visual_names = visual_names_A + visual_names_B  # combine visualizations for A and B
         # specify the models you want to save to the disk. The training/test scripts will call <BaseModel.save_networks> and <BaseModel.load_networks>.
@@ -109,12 +120,32 @@ class CycleGANModel(BaseModel):
         self.real_B = input['B' if AtoB else 'A'].to(self.device)
         self.image_paths = input['A_paths' if AtoB else 'B_paths']
 
+        if self.isTrain:
+            self.rand= random.random()
+            self.B_txt_path = input['B_txt_paths'] if 'B_txt_paths' in input.keys() else ['']
+            self.B_txt_polys = pred_reading_mod(self.B_txt_path) if self.opt.dataset_mode == 'alignedText' else []
+            self.mask = torch.ones(self.real_B.shape[2:4], dtype=torch.int64)
+            for poly in self.B_txt_polys:
+                poly_coordinates = torch.Tensor(list(poly.exterior.coords))
+                y, x = polygon(poly_coordinates[:, 0], poly_coordinates[:, 1], self.real_B.shape[2:4])
+                self.mask[x, y] = 0
+            self.mask_full = self.mask.clone().to(self.device)
+            self.mask_full = self.mask_full[None][None].repeat(1, 3, 1, 1)
+
+
     def forward(self):
         """Run forward pass; called by both functions <optimize_parameters> and <test>."""
         self.fake_B = self.netG_A(self.real_A)  # G_A(A)
         self.rec_A = self.netG_B(self.fake_B)   # G_B(G_A(A))
         self.fake_A = self.netG_B(self.real_B)  # G_B(B)
         self.rec_B = self.netG_A(self.fake_A)   # G_A(G_B(B))
+        if self.isTrain:
+            self.fake_BM = self.fake_B * self.mask_full
+            self.real_BM = self.real_B * self.mask_full
+            self.rec_BM = self.rec_B * self.mask_full
+            self.M = self.mask_full
+
+
 
     def backward_D_basic(self, netD, real, fake):
         """Calculate GAN loss for the discriminator
@@ -141,7 +172,8 @@ class CycleGANModel(BaseModel):
     def backward_D_A(self):
         """Calculate GAN loss for discriminator D_A"""
         fake_B = self.fake_B_pool.query(self.fake_B)
-        self.loss_D_A = self.backward_D_basic(self.netD_A, self.real_B, fake_B)
+        self.loss_D_A = self.backward_D_basic(self.netD_A, self.real_B * self.mask_full, fake_B * self.mask_full)
+
 
     def backward_D_B(self):
         """Calculate GAN loss for discriminator D_B"""
@@ -154,27 +186,35 @@ class CycleGANModel(BaseModel):
         lambda_A = self.opt.lambda_A
         lambda_B = self.opt.lambda_B
         # Identity loss
+
         if lambda_idt > 0:
+            #don't change here
             # G_A should be identity if real_B is fed: ||G_A(B) - B||
             self.idt_A = self.netG_A(self.real_B)
-            self.loss_idt_A = self.criterionIdt(self.idt_A, self.real_B) * lambda_B * lambda_idt
+            self.loss_idt_A = self.criterionIdt(self.mask_full*self.idt_A, self.real_B*self.mask_full) * lambda_B * lambda_idt*0
+            self.idt_AM = self.mask_full*self.idt_A
             # G_B should be identity if real_A is fed: ||G_B(A) - A||
             self.idt_B = self.netG_B(self.real_A)
-            self.loss_idt_B = self.criterionIdt(self.idt_B, self.real_A) * lambda_A * lambda_idt
+            self.loss_idt_B = self.criterionIdt(self.idt_B, self.real_A) * lambda_A * lambda_idt*0
+
         else:
             self.loss_idt_A = 0
             self.loss_idt_B = 0
 
-        # GAN loss D_A(G_A(A))
-        self.loss_G_A = self.criterionGAN(self.netD_A(self.fake_B), True)
-        # GAN loss D_B(G_B(B))
+        # GAN loss D_A(G_A(A)*M)
+        self.loss_G_A = self.criterionGAN(self.netD_A(self.fake_B*self.mask_full), True)
+        # GAN loss D_B(G_B(B))i
         self.loss_G_B = self.criterionGAN(self.netD_B(self.fake_A), True)
         # Forward cycle loss || G_B(G_A(A)) - A||
         self.loss_cycle_A = self.criterionCycle(self.rec_A, self.real_A) * lambda_A
-        # Backward cycle loss || G_A(G_B(B)) - B||
-        self.loss_cycle_B = self.criterionCycle(self.rec_B, self.real_B) * lambda_B
-        # combined loss and calculate gradients
-        self.loss_G = self.loss_G_A + self.loss_G_B + self.loss_cycle_A + self.loss_cycle_B + self.loss_idt_A + self.loss_idt_B
+        # Backward cycle loss || G_A(G_B(B))*M - B*M||
+        self.loss_cycle_B = self.criterionCycle(self.rec_B*self.mask_full, self.real_B*self.mask_full) * lambda_B
+        # aligned loss
+        self.loss_G_aligned_A = self.criterionCycle(self.fake_A, self.real_A) * lambda_A
+        self.loss_G_aligned_B = self.criterionCycle(self.fake_B * self.mask_full, self.real_B * self.mask_full) * lambda_B
+
+        # combine loss and calculate gradients
+        self.loss_G = self.loss_G_aligned_A + self.loss_G_aligned_B + self.loss_cycle_A + self.loss_cycle_B + self.loss_G_B + self.loss_G_A
         self.loss_G.backward()
 
     def optimize_parameters(self):
@@ -192,3 +232,22 @@ class CycleGANModel(BaseModel):
         self.backward_D_A()      # calculate gradients for D_A
         self.backward_D_B()      # calculate graidents for D_B
         self.optimizer_D.step()  # update D_A and D_B's weights
+
+
+def pred_reading_mod(input_dir):
+    """This helper reads input from txt files"""
+    try:
+        with open('%s' % (input_dir[0]), 'r') as input_fid:
+            pred = input_fid.read().rstrip()
+
+    except FileNotFoundError as e:
+        print('text file with text countours is not found')
+        sys.exit(1)
+    pred_polys = []
+    pred = pred.split('\n')
+    for i in range(len(pred)):
+        x = np.array(pred[i].split(' '))
+        x = np.array(x).reshape(-1, 2)
+        x = np.append(x, [x[0]], axis=0)
+        pred_polys.append(Polygon(x.astype(float).tolist()))
+    return pred_polys
